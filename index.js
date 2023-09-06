1b17ed8ef26d1e9bc7ca0aa9d22856baeddba5a1
islom <kilhom606@gmail.com>
2023-09-06 14:51:30 +0500

    rel

---
+++ index.js
@@ -0,0 +1,13 @@
+let btns = document.querySelectorAll('button [ data-h1]')
+let h1 = document.querySelector('h1')
+
+let h11 = {
+    " img/image-3.png " : " 65 км/ч "
+}
+
+btns.forEach(btn => {
+    btn.onclick = () => {
+        let text = btn.getAttribute('data-h1')
+       
+    }
+})
